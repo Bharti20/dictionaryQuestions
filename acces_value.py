7f504795441d671fbb27108ae5e30d2fe4623dc3
Bharti20 <bhartikumari20@navgurukul.org>
2020-12-11 00:11:23 +0800

    solve some dictionary questions

---
+++ acces_value.py
@@ -0,0 +1,4 @@
+my_dic={"Bharti":"kumari", "nav":"Banglore","sandhya":5}
+print(my_dic["nav"])
+print(my_dic)
+print(type(my_dic))
---
+++ create_dictionary.py
@@ -0,0 +1,5 @@
+my_dic= dict(name="Bharti", village="mohamamari", state="Bihar")
+print(my_dic["name"])
+print(my_dic["village"])
+print(my_dic["state"])
+print(my_dic.values())
